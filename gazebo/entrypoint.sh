d203ee7f46584a0ede33284a7aedc6978c5feef3
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-04-16 12:06:48 +0200

    Update entrypoint.sh added mesh path

--- gazebo/entrypoint.sh
+++ gazebo/entrypoint.sh
@@ -7,4 +7,6 @@ if [ -f /ws/install/setup.bash ]; then
   source /ws/install/setup.bash
 fi
 
+export GZ_SIM_RESOURCE_PATH=/ws/src/floribot_gz_description:/ws/src/floribot_gz_description/meshes:${GZ_SIM_RESOURCE_PATH}
+
 exec "$@"

ddd987bb7aace6422771af276a1f7429c786691e
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-04-16 13:54:59 +0200

    Update spawn_floribot.launch.py

--- gazebo/ros2_ws/src/floribot_gz_bringup/launch/spawn_floribot.launch.py
+++ gazebo/ros2_ws/src/floribot_gz_bringup/launch/spawn_floribot.launch.py
@@ -1,14 +1,25 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 from virtual_maize_field import get_spawner_launch_file
 
 
 def generate_launch_description():
+    robot_model = PathJoinSubstitution([
+        FindPackageShare("floribot_gz_description"),
+        "models",
+        "floribot_test.sdf",
+    ])
+
     spawn_robot = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([get_spawner_launch_file()]),
         launch_arguments={
-            "robot_name": "floribot4",
+            "world": "virtual_maize_field",
+            "file": robot_model,
+            "entity_name": "floribot4",
+            "allow_renaming": "False",
         }.items(),
     )
 

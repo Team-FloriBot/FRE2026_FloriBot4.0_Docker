17dce6c36e53a744cb6724e3c1d8b27380d024b8
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-04-29 20:26:34 +0200

    Update spawn_floribot.launch.py

--- gazebo/ros2_ws/src/floribot_gz_bringup/launch/spawn_floribot.launch.py
+++ gazebo/ros2_ws/src/floribot_gz_bringup/launch/spawn_floribot.launch.py
@@ -8,10 +8,10 @@ def generate_launch_description():
             "bash", "-lc",
             "source /opt/ros/jazzy/setup.bash && "
             "source /ws/install/setup.bash && "
-            "xacro /ws/src/floribot_gz_description/urdf/Floribot_gz.urdf.xacro > /tmp/floribot_gz.urdf && "
+            "xacro xacro /ws/src/robot_description/src/urdf/Floribot.urdf.xacro > /tmp/floribot_gz.urdf && "
             "ros2 run ros_gz_sim create "
             "-world virtual_maize_field "
-            "-name floribot4 "
+            "-name FloriBot "
             "-file /tmp/floribot_gz.urdf "
             "-x -1.3631752758808973 "
             "-y -3.460156696047507 "

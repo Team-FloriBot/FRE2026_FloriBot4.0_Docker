0df332d3e6e44c141fe041b9ff06386977e9f293
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-08 09:15:33 +0200

    Update start_sensors.sh

--- sensors/start_sensors.sh
+++ sensors/start_sensors.sh
@@ -15,6 +15,14 @@ SICK_REAR_FRAME="${SICK_REAR_FRAME:-sick_rear_link}"
 RS_FRONT_SERIAL="${RS_FRONT_SERIAL:-947522071563}"
 RS_REAR_SERIAL="${RS_REAR_SERIAL:-}"
 
+XSENS_ENABLE="${XSENS_ENABLE:-true}"
+XSENS_SCAN_FOR_DEVICES="${XSENS_SCAN_FOR_DEVICES:-true}"
+XSENS_PORT="${XSENS_PORT:-/dev/ttyUSB0}"
+XSENS_BAUDRATE="${XSENS_BAUDRATE:-115200}"
+XSENS_FRAME_ID="${XSENS_FRAME_ID:-xsens_mti_link}"
+XSENS_NAMESPACE="${XSENS_NAMESPACE:-/sensors/xsens}"
+XSENS_LOG_LEVEL="${XSENS_LOG_LEVEL:-info}"
+
 pids=()
 
 ros2 launch sick_scan_xd "${SICK_LAUNCH_FILE}" \
@@ -52,6 +60,22 @@ if [ -n "${RS_REAR_SERIAL}" ]; then
     enable_color:=true \
     enable_depth:=true \
     align_depth.enable:=true &
+  pids+=($!)
+fi
+
+if [ "${XSENS_ENABLE}" = "true" ] || [ "${XSENS_ENABLE}" = "1" ]; then
+  xsens_args=(
+    --ros-args
+    --params-file "$(ros2 pkg prefix xsens_mti_ros2_driver)/share/xsens_mti_ros2_driver/param/xsens_mti_node.yaml"
+    -r __ns:="${XSENS_NAMESPACE}"
+    -p scan_for_devices:="${XSENS_SCAN_FOR_DEVICES}"
+    -p port:="${XSENS_PORT}"
+    -p baudrate:="${XSENS_BAUDRATE}"
+    -p frame_id:="${XSENS_FRAME_ID}"
+    --log-level "${XSENS_LOG_LEVEL}"
+  )
+
+  ros2 run xsens_mti_ros2_driver xsens_mti_node "${xsens_args[@]}" &
   pids+=($!)
 fi
 

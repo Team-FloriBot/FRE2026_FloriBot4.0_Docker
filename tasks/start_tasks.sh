613f96d44c56532de2346234016581390f793c3f
norikaschneider <norikaschneider@gmail.com>
2026-05-13 10:43:39 +0200

    use system slam toolbox

--- tasks/start_tasks.sh
+++ tasks/start_tasks.sh
@@ -10,7 +10,8 @@ echo "Starte Laser Scan Merger..."
 ros2 launch laser_scan_merger start.launch.py robotname:=floribot_config &
 
 echo "Starte SLAM Toolbox..."
-ros2 launch slam_toolbox online_sync_launch.py &
+ros2 launch slam_toolbox online_sync_launch.py \
+    slam_params_file:=/ws/src/FRE2026_Tasks/slam_toolbox/config/mapper_params_online_sync.yaml &
 
 #echo "Starte Maize Navigaion..."
 #ros2 launch maize_navigation maize_navigation.launch.py &

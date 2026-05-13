f39219c6a4104d05e481a956c5c2fb4ca91201a9
norikaschneider <norikaschneider@gmail.com>
2026-05-13 10:31:48 +0200

    add slam_toolbox

--- tasks/start_tasks.sh
+++ tasks/start_tasks.sh
@@ -9,8 +9,8 @@ fi
 echo "Starte Laser Scan Merger..."
 ros2 launch laser_scan_merger start.launch.py robotname:=floribot_config &
 
-#echo "Starte SLAM Toolbox..."
-#ros2 launch slam_toolbox online_sync_launch.py &
+echo "Starte SLAM Toolbox..."
+ros2 launch slam_toolbox online_sync_launch.py &
 
 #echo "Starte Maize Navigaion..."
 #ros2 launch maize_navigation maize_navigation.launch.py &

ababfd5857cd175b24ca88379b90113356c366b8
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-13 18:32:57 +0200

    Update start_tasks.sh

--- tasks/start_tasks.sh
+++ tasks/start_tasks.sh
@@ -11,7 +11,7 @@ ros2 launch laser_scan_merger start.launch.py robotname:=floribot_config &
 
 echo "Starte SLAM Toolbox..."
 ros2 launch slam_toolbox online_sync_launch.py \
-    slam_params_file:=/ws/src/FRE2026_Tasks/slam_toolbox/config/mapper_params_online_sync.yaml &
+    slam_params_file:=/ws/src/FRE2026_Tasks/src/slam_toolbox/config/mapper_params_online_sync.yaml &
 
 echo "Starte Maize Navigaion..."
 ros2 launch maize_navigation maize_navigation.launch.py &

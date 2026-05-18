0eba17df43989178d264f7cd58769223bf97a8f3
norikaschneider <norika.schneider@gmail.com>
2026-05-18 10:00:17 +0200

    Fix parameter names in SICK lidar launch commands

--- sensors/start_sensors.sh
+++ sensors/start_sensors.sh
@@ -38,10 +38,10 @@ trap shutdown SIGINT SIGTERM
 echo "[sensors] Starting SICK front lidar on ${SICK_FRONT_IP}"
 ros2 launch sick_scan_xd "${SICK_LAUNCH_FILE}" \
   hostname:="${SICK_FRONT_IP}" \
-  nodename:=sick_front \
+  node_name:=sick_front \
   frame_id:="${SICK_FRONT_FRAME}" \
   tf_publish_rate:=0 \
-  use_generation_timestamp:=false \
+  ros_timestamp_control:=0 \
   laserscan_topic:=/sensors/scan_front \
   cloud_topic:=/sensors/cloud_front &
 pids+=($!)
@@ -49,10 +49,10 @@ pids+=($!)
 echo "[sensors] Starting SICK rear lidar on ${SICK_REAR_IP}"
 ros2 launch sick_scan_xd "${SICK_LAUNCH_FILE}" \
   hostname:="${SICK_REAR_IP}" \
-  nodename:=sick_rear \
+  node_name:=sick_rear \
   frame_id:="${SICK_REAR_FRAME}" \
   tf_publish_rate:=0 \
-  use_generation_timestamp:=false \
+  ros_timestamp_control:=0 \
   laserscan_topic:=/sensors/scan_rear \
   cloud_topic:=/sensors/cloud_rear &
 pids+=($!)

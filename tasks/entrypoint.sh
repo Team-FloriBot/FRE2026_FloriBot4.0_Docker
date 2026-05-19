2711f1221e7273584ccf41f8b2cc70bfc65b395b
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-05-19 22:56:10 +0200

    Update entrypoint.sh

--- tasks/entrypoint.sh
+++ tasks/entrypoint.sh
@@ -7,5 +7,4 @@ if [ -f /ws/install/setup.bash ]; then
     source /ws/install/setup.bash
 fi
 
-echo "Starte Maize Navigation..."
-exec ros2 launch maize_navigation maize_navigation.launch.py
+exec "$@"

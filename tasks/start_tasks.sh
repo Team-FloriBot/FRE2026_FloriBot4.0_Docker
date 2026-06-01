d95e1eafdacd37edc051e1e6f104dcfa08d487fb
norikaschneider <norika.schneider@gmail.com>
2026-06-01 15:14:00 +0200

    checkout tasks

--- tasks/start_tasks.sh
+++ tasks/start_tasks.sh
@@ -7,8 +7,13 @@ if [ -f /ws/install/setup.bash ]; then
     source /ws/install/setup.bash
 fi
 
-echo "Starte Maize Navigation..."
-exec ros2 launch maize_navigation maize_navigation.launch.py
+# echo "Starte Maize Navigation..."
+# ros2 launch maize_navigation maize_navigation.launch.py &
 
 echo "Starte Task 4 (Marker Detection, Global Marker Mapping, Coverage Planner)..."
-exec ros2 launch task4 task4.launch.py
+ros2 launch task4 task4.launch.py &
+
+echo "Starte Path Tracking Controller..."
+ros2 launch path_tracking_controller path_tracking_controller.launch.py &
+
+wait
